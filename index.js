5f9623ccf24522d9c3e4c4781fbc741b7ad22962
vatsal <mavanivatsal10@gmail.com>
2024-12-10 21:09:28 +0530

    updateUI

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const logger = (req, res, next) => {
 };
 app.use(logger);
 app.use(cors());
+app.use(express.static("dist"));
 
 let persons = [
   {
@@ -78,4 +79,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-app.listen(3001);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
